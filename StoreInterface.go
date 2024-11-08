9a7f61d881458ca8a4ae1e3e72160a6ec7de2e41
Sinevia <info@sinevia.com>
2024-11-08 08:30:11 +0000

    updated interface

--- StoreInterface.go
+++ StoreInterface.go
@@ -4,6 +4,7 @@ type StoreInterface interface {
 	AutoMigrate() error
 	EnableDebug(debug bool)
 	UserCreate(user UserInterface) error
+	UserCount(options UserQueryInterface) (int64, error)
 	UserDelete(user UserInterface) error
 	UserDeleteByID(id string) error
 	UserFindByEmail(email string) (UserInterface, error)
